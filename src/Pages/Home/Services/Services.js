8b648b93d83a6871535f21dcbf14c5cb08c52f52
Zamal Hossain <thezamalian@gmail.com>
2021-10-31 20:15:04 +0600

    DB is added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import SingleService from '../SingleService/SingleService';
 
+const services = [
+    {
+        name: "Family Tour",
+        image: "https://i.ibb.co/Y7wvvq3/img-card-12.jpg",
+        detail: "Small travellers, big fun. If one family member isn't having a good time, nobody is. We give each Family tour a Recommended Age rank so parents."
+    },
+    {
+        name: "Couple Tour",
+        image: "https://i.ibb.co/87fqLWT/img-card-13.jpg",
+        detail: "One of the best parts of being in a relationship is having a built-in travel buddy ... A great way to bond as a couple is by getting active"
+    },
+    {
+        name: "Adventure Tour",
+        image: "https://i.ibb.co/c6L7f2S/img-card-4.jpg",
+        detail: "Discover the Great Barrier Reef and Green Island with Great Adventures Cruises. With a choice of day trips to beautiful Green Island and to the spectacular."
+    },
+
+    {
+        name: "Relaxing Special",
+        image: "https://i.ibb.co/9W29bPp/img-card-5.jpg",
+        detail: "Maybe the most well-known and the best relaxing destination in the world is Bali. This tranquil island in Indonesia is known for being."
+    },
+    {
+        name: "Family Adventure",
+        image: "https://i.ibb.co/2k5VQsJ/img-card-7.jpg",
+        detail: "Family trips are a great way for children to meet and interact with other kids from around the world who are close to their own age and share common interests."
+    },
+    {
+        name: "Great Honeymoon",
+        image: "https://i.ibb.co/w44MyvG/img-card-8.jpg",
+        detail: "Honeymoon destination ideas for a once-in-a-lifetime escape. ... Tom Merchant is the co-founder of luxury travel company and trip planner."
+    },
+]
+
 const Services = () => {
     return (
         <div className='mt-5 mx-3'>
@@ -8,13 +42,9 @@ const Services = () => {
 
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
 
-                <SingleService></SingleService>
-                <SingleService></SingleService>
-                <SingleService></SingleService>
-
-                <SingleService></SingleService>
-                <SingleService></SingleService>
-                <SingleService></SingleService>
+                {
+                    services.map(service => <SingleService key={service.name} service={service} ></SingleService>)
+                }
 
             </div>
         </div>
--- src/Pages/Home/SingleService/SingleService.js
+++ src/Pages/Home/SingleService/SingleService.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import burjKhalifa from '../../../images/burj-khalifa.jpg'
-    ;
-const SingleService = () => {
+// import burjKhalifa from '../../../images/burj-khalifa.jpg'
+;
+
+const SingleService = ({ service }) => {
+    const { name, image, detail } = service;
+
     return (
         <div>
             <div className="col">
                 <div className="card h-100">
-                    <img src={burjKhalifa} className="card-img-top " alt="..." style={{ height: '30vw' }} />
+                    <img src={image} className="card-img-top " alt="..." style={{ height: '30vw' }} />
                     <div className="card-body">
-                        <h5 className="card-title">Service Name</h5>
-                        <p className="card-text">Short Description :: This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">{detail}</p>
 
                         <Link to="/book-now">
                             <button className='btn btn-primary w-100'>Book Now</button>
